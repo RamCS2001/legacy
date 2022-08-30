678800215b53adf202ca674a818080c43a924ff7
Ramkumar Karthikeyan <kram.cse.2001@gmail.com>
2022-08-30 19:25:35 +0530

    LINK BUG

--- src/app/db-utility.service.ts
+++ src/app/db-utility.service.ts
@@ -139,7 +139,7 @@ export class DbUtilityService {
   }
 
 
-  getParticipantNumberCount= "http://legacymepco.herokuapp.com/getParticipantNumberCount"
+  getParticipantNumberCount= "httpS://legacymepco.herokuapp.com/getParticipantNumberCount"
   getParticipantsCount(){
     return this.http.get(this.getParticipantNumberCount)
   }
